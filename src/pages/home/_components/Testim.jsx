6db1a609bb030dd79812d50d91a1506b42fcd0d6
BGad240 <bgad240@gmail.com>
2025-10-01 22:00:31 +0300

    fixing problems4

--- src/pages/home/_components/Testim.jsx
+++ src/pages/home/_components/Testim.jsx
@@ -49,7 +49,7 @@ export default function TestimonialsSlider() {
               exit={{ opacity: 0, y: -40 }}
               transition={{ duration: 0.6 }}
             >
-              <Card className="shadow-xl rounded-2xl bg-white border border-gray-100">
+              <Card className="shadow-xl rounded-2xl bg-white border border-gray-100 py-[20px]">
                 <CardContent className="p-10 flex flex-col items-center">
                   {/* صورة المستخدم (Avatar) */}
                   <img
